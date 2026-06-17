d4b06be93e6141be685bbd5010b77d0048d6f9ec
ARTHUR SOUSA SANTOS <025864@fatecararas.com>
2026-06-17 12:16:36 -0300

    pt3 completa

--- pjl2/polls/views.py
+++ pjl2/polls/views.py
@@ -1,17 +1,14 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá mundo")
-
+    return render(request, "polls/index.html")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    return HttpResponse(f"Você está vendo a questão {question_id}.")
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    return HttpResponse(f"Resultados da questão {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse(f"Votando na questão {question_id}.")
